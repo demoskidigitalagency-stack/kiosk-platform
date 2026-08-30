05a878c5d1ac56201ff6ea40b0fa6611ef3f44ff
demoskidigitalagency-stack <demoski.digital.agency@gmail.com>
2026-08-30 17:01:02 +0100

    fix: remove flat all modules navigation installer

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { installAppearanceNomenclature } from './appearance-nomenclature';
-import { installAllModulesNavigation } from './all-modules-navigation';
 import './unified-shell.css';
 import './appearance-shell-correction.css';
 import './appearance-system.css';
 import './appearance-system';
 
 installAppearanceNomenclature();
-installAllModulesNavigation();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
