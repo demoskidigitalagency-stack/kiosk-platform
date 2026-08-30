bd582ed508d4791c64f20ae655878b4aa0f7456d
demoskidigitalagency-stack <demoski.digital.agency@gmail.com>
2026-08-30 16:31:13 +0100

    Load corrected KIOSK appearance shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { installAppearanceNomenclature } from './appearance-nomenclature';
 import './unified-shell.css';
+import './appearance-shell-correction.css';
+
+installAppearanceNomenclature();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
